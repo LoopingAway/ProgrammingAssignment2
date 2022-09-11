6795ec9162e0d8443b444011d0a4369bf732646d
LoopingAway <93289661+LoopingAway@users.noreply.github.com>
2022-09-11 18:55:49 +0200

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## Function to create a matrix object and cache its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        trevni <- NULL
+        set <- function(y) {
+            x <<- y
+            trevni <<- NULL
+        }
+        get <- function() x
+        setinverse <- function(inverse) trevni <<- inverse
+        getinverse <- function() trevni
+        list(set = set, get = get,
+             setinverse = setinverse,
+             getinverse = getinverse)
+    
 }
 
 
-## Write a short comment describing this function
-
+## Check if Inverse of matrix is stored in Variable trevni, if so return Value, 
+## Otherwise Message that matrix is NULL, get matrix, Solve matrix, Return inverted matrix 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+        trevni <- x$getinverse()
+        if(!is.null(trevni)) {
+            message("Matrix hasn't changed - no solving")
+            return(trevni)
+        } else {
+            message("Matrix has changed - solving matrix again")
+            data <- x$get()
+            trevni <- solve(data, ...)
+            x$setinverse(trevni)
+            trevni
+        }  
+    }
